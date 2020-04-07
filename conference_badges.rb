e8267e6945bc86e2b3c4c2bcaf74b3942d27d4f8
Jack Cooper <jackcooper1245@gmail.com>
2020-04-07 14:21:13 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,6 +19,8 @@ def assign_rooms(speakers)
 end
 
 def printer(attendees)
- batch_badge_creator(attendees)
- assign_rooms(attendees)
+ batch_badge_creator(attendees).each do |output|
+ assign_rooms(attendees).each do |output1|
+   puts output + output1
+ end
 end
